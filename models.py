e89e0d26707b5b0da8bda5b211a868a44a8a3f8a
Jojof <496903741@qq.com>
2020-04-06 11:19:29 +0800

    用户模型添加密码注册方法

--- models.py
+++ models.py
@@ -7,6 +7,8 @@ import os
 from datetime import datetime
 
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from extensions import db
 
 
@@ -20,4 +22,5 @@ class User(db.Model, UserMixin):
     # 用户状态
     confirmed = db.Column(db.Boolean, default=False)
 
-
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
