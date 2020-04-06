9d14ccbfac875fef82029e3312c5855b03b0225d
Jojof <496903741@qq.com>
2020-04-06 11:21:26 +0800

    设置文件注册并添加Operation

---
+++ settings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    :author: Jifan Jiang
+    :url: https://github.com/fn19980304
+"""
+import os
+import sys
+
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+prefix = 'sqlite:///'
+
+
+class Operations:
+    CONFIRM = 'confirm'
