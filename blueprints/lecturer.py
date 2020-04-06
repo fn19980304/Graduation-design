7802ac48ca214864e159332ea6beb49fb6d48a11
Jojof <496903741@qq.com>
2020-04-06 12:14:24 +0800

    移动文件

--- blueprints/lecturer.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-    :author: Jifan Jiang
-    :url: https://github.com/fn19980304
-"""
--- extensions.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-    :author: Jifan Jiang
-    :url: https://github.com/fn19980304
-"""
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
