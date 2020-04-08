16936ec23e063840f3397cd1481f73cc4f60a7e6
Jojof <496903741@qq.com>
2020-04-08 12:23:01 +0800

    蓝本文件main略微改动

--- devGrasys/blueprints/main.py
+++ devGrasys/blueprints/main.py
@@ -5,7 +5,7 @@
 """
 import os
 
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template
 from devGrasys.extensions import db
 
 main_bp = Blueprint('main', __name__)
