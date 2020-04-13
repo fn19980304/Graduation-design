475e87391249355bae70647c9bae89810d22deda
Jojof <496903741@qq.com>
2020-04-13 20:31:07 +0800

    增加讲师、学生主页课程列表显示，增加相应课程页面加入及退出课程功能

--- devGrasys/blueprints/assistant.py
+++ devGrasys/blueprints/assistant.py
@@ -4,10 +4,10 @@
     :url: https://github.com/fn19980304
 """
 
-from flask import render_template, flash, redirect, url_for, Blueprint
-from flask_login import login_user, current_user
+from flask import render_template, flash, redirect, url_for, Blueprint, send_from_directory, current_app
+from flask_login import login_user, current_user, login_required, logout_user
 
-from devGrasys.models import Assistant
+from devGrasys.models import Assistant, Course, Lecturer
 from devGrasys.forms.assistant import RegisterFormAssistant, LoginFormAssistant
 from devGrasys.utils import redirect_back
 from devGrasys.extensions import db
@@ -15,15 +15,19 @@ from devGrasys.extensions import db
 assistant_bp = Blueprint('assistant', __name__)
 
 
-@assistant_bp.route('/student', methods=['GET'])
+@assistant_bp.route('/', methods=['GET'])
 def index_assistant():
+    if current_user.is_authenticated:
+        courses = Course.query.all()
+        lecturers = Lecturer.query.all()
+        return render_template('assistant/index_assistant.html', courses=courses, lecturers=lecturers)
     return render_template('assistant/index_assistant.html')
 
 
-@assistant_bp.route('/assistant/login', methods=['GET', 'POST'])
+@assistant_bp.route('/login', methods=['GET', 'POST'])
 def login_assistant():
     if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
+        return redirect(url_for('assistant.index_assistant'))
 
     form = LoginFormAssistant()
     if form.validate_on_submit():
@@ -32,18 +36,23 @@ def login_assistant():
             if login_user(user, form.remember_me.data):
                 flash('Login success.', 'info')
                 return redirect_back()
-            else:
-                flash('Your account is blocked.', 'warning')
-                return redirect(url_for('main.index'))
         flash('Invalid userID or password.', 'warning')
 
     return render_template('assistant/login_assistant.html', form=form)
 
 
-@assistant_bp.route('/assistant/register', methods=['GET', 'POST'])
+@assistant_bp.route('/logout')
+@login_required
+def logout_assistant():
+    logout_user()
+    flash('Logout success.', 'info')
+    return redirect(url_for('main.index'))
+
+
+@assistant_bp.route('/register', methods=['GET', 'POST'])
 def register_assistant():
     if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
+        return redirect(url_for('assistant.index_assistant'))
 
     form = RegisterFormAssistant()
     if form.validate_on_submit():
@@ -54,5 +63,43 @@ def register_assistant():
         user.set_password(password)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('.login_assistant'))
+        return redirect(url_for('assistant.login_assistant'))
     return render_template('assistant/register_assistant.html', form=form)
+
+
+@assistant_bp.route('/avatars/<path:filename>')
+def get_avatar(filename):
+    return send_from_directory(current_app.config['AVATARS_SAVE_PATH'], filename)
+
+
+@assistant_bp.route('/<course_name>')
+def view_course(course_name):
+    assistant = current_user
+    course = Course.query.filter_by(name=course_name).first_or_404()
+    return render_template('assistant/view_course.html', course=course, assistant=assistant)
+
+
+@assistant_bp.route('/<course_name>/join', methods=['POST'])
+def join_course(course_name):
+    assistant = current_user
+    course = Course.query.filter_by(name=course_name).first_or_404()
+    if course.assistant_is_joined(assistant=assistant):
+        flash('Already joined.', 'info')
+        return redirect_back()
+
+    course.assistant_join(assistant)
+    flash('Course joined.', 'success')
+    return redirect_back()
+
+
+@assistant_bp.route('/<course_name>/quit', methods=['POST'])
+def quit_course(course_name):
+    assistant = current_user
+    course = Course.query.filter_by(name=course_name).first_or_404()
+    if not course.assistant_is_joined(assistant=assistant):
+        flash('Not joined yet.', 'info')
+        return redirect_back()
+
+    course.assistant_quit(assistant)
+    flash('Course quoted.', 'success')
+    return redirect_back()
--- devGrasys/blueprints/student.py
+++ devGrasys/blueprints/student.py
@@ -4,10 +4,10 @@
     :url: https://github.com/fn19980304
 """
 
-from flask import render_template, flash, redirect, url_for, Blueprint
-from flask_login import login_user, current_user
+from flask import render_template, flash, redirect, url_for, Blueprint, send_from_directory, current_app
+from flask_login import login_user, current_user, login_required, logout_user
 
-from devGrasys.models import Student
+from devGrasys.models import Student, Course, Lecturer
 from devGrasys.forms.student import RegisterFormStudent, LoginFormStudent
 from devGrasys.utils import redirect_back
 from devGrasys.extensions import db
@@ -15,15 +15,19 @@ from devGrasys.extensions import db
 student_bp = Blueprint('student', __name__)
 
 
-@student_bp.route('/student', methods=['GET'])
+@student_bp.route('', methods=['GET'])
 def index_student():
+    if current_user.is_authenticated:
+        courses = Course.query.all()
+        lecturers = Lecturer.query.all()
+        return render_template('student/index_student.html', courses=courses, lecturers=lecturers)
     return render_template('student/index_student.html')
 
 
-@student_bp.route('/student/login', methods=['GET', 'POST'])
+@student_bp.route('/login', methods=['GET', 'POST'])
 def login_student():
     if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
+        return redirect(url_for('student.index_student'))
 
     form = LoginFormStudent()
     if form.validate_on_submit():
@@ -32,18 +36,23 @@ def login_student():
             if login_user(user, form.remember_me.data):
                 flash('Login success.', 'info')
                 return redirect_back()
-            else:
-                flash('Your account is blocked.', 'warning')
-                return redirect(url_for('main.index'))
         flash('Invalid userID or password.', 'warning')
 
     return render_template('student/login_student.html', form=form)
 
 
-@student_bp.route('/student/register', methods=['GET', 'POST'])
+@student_bp.route('/logout')
+@login_required
+def logout_student():
+    logout_user()
+    flash('Logout success.', 'info')
+    return redirect(url_for('main.index'))
+
+
+@student_bp.route('/register', methods=['GET', 'POST'])
 def register_student():
     if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
+        return redirect(url_for('student.index_student'))
 
     form = RegisterFormStudent()
     if form.validate_on_submit():
@@ -54,5 +63,43 @@ def register_student():
         user.set_password(password)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('.login_student'))
+        return redirect(url_for('student.login_student'))
     return render_template('student/register_student.html', form=form)
+
+
+@student_bp.route('/avatars/<path:filename>')
+def get_avatar(filename):
+    return send_from_directory(current_app.config['AVATARS_SAVE_PATH'], filename)
+
+
+@student_bp.route('/<course_name>')
+def view_course(course_name):
+    student = current_user
+    course = Course.query.filter_by(name=course_name).first_or_404()
+    return render_template('student/view_course.html', course=course, student=student)
+
+
+@student_bp.route('/<course_name>/join', methods=['POST'])
+def join_course(course_name):
+    student = current_user
+    course = Course.query.filter_by(name=course_name).first_or_404()
+    if course.student_is_joined(student=student):
+        flash('Already joined.', 'info')
+        return redirect_back()
+
+    course.student_join(student)
+    flash('Course joined.', 'success')
+    return redirect_back()
+
+
+@student_bp.route('/<course_name>/quit', methods=['POST'])
+def quit_course(course_name):
+    student = current_user
+    course = Course.query.filter_by(name=course_name).first_or_404()
+    if not course.student_is_joined(student=student):
+        flash('Not joined yet.', 'info')
+        return redirect_back()
+
+    course.student_quit(student)
+    flash('Course quoted.', 'success')
+    return redirect_back()
