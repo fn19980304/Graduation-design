ea585ceb2521b7b0a9b6337d0c761ca7ecca5609
Jojof <496903741@qq.com>
2020-04-13 20:33:34 +0800

    为课程对学生及讲师建立多对多联系，并添加相应操作

--- devGrasys/models.py
+++ devGrasys/models.py
@@ -13,6 +13,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from devGrasys.extensions import db
 
+course_student_table = db.Table('course_student', db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
+                                db.Column('student_id', db.Integer, db.ForeignKey('student.id'))
+                                )
+
+course_assistant_table = db.Table('course_assistant', db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
+                                  db.Column('assistant_id', db.Integer, db.ForeignKey('assistant.id'))
+                                  )
+
 
 class Student(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,7 +28,7 @@ class Student(db.Model, UserMixin):
     password_hash = db.Column(db.String(128))
     name = db.Column(db.String(30))
 
-    # classes = db.relationship('Class', secondary=student_class_table, back_populates='students')
+    courses = db.relationship('Course', secondary=course_student_table, back_populates='students', lazy='dynamic')
 
     def get_id(self):
         return 'student.' + str(self.id)
@@ -38,7 +46,7 @@ class Assistant(db.Model, UserMixin):
     password_hash = db.Column(db.String(128))
     name = db.Column(db.String(30))
 
-    # classes = db.relationship('Class', secondary=assistant_class_table, back_populates='assistants')
+    courses = db.relationship('Course', secondary=course_assistant_table, back_populates='assistants', lazy='dynamic')
 
     def get_id(self):
         return 'assistant.' + str(self.id)
@@ -71,11 +79,23 @@ class Lecturer(db.Model, UserMixin):
 class Course(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
+    lecturer_name = db.Column(db.String(30))
     intro = db.Column(db.Text)
 
     avatar_s = db.Column(db.String(64))
     avatar_m = db.Column(db.String(64))
     avatar_l = db.Column(db.String(64))
+
+    # 讲师对课程：一对多
+    lecturer_id = db.Column(db.Integer, db.ForeignKey('lecturer.id'))
+    lecturer = db.relationship('Lecturer', back_populates='courses')
+
+    # 助教对课程：多对多
+    assistants = db.relationship('Assistant', secondary=course_assistant_table, back_populates='courses',
+                                 lazy='dynamic')
+
+    # 学生对课程：多对多
+    students = db.relationship('Student', secondary=course_student_table, back_populates='courses', lazy='dynamic')
 
     def __init__(self, **kwargs):
         super(Course, self).__init__(**kwargs)
@@ -89,6 +109,28 @@ class Course(db.Model, UserMixin):
         self.avatar_l = filenames[2]
         db.session.commit()
 
-    # 讲师对课程：一对多
-    lecturer_id = db.Column(db.Integer, db.ForeignKey('lecturer.id'))
-    lecturer = db.relationship('Lecturer', back_populates='courses')
+    def student_join(self, student):
+        if not self.student_is_joined(student):
+            self.students.append(student)
+            db.session.commit()
+
+    def student_quit(self, student):
+        if self.student_is_joined(student):
+            self.students.remove(student)
+            db.session.commit()
+
+    def student_is_joined(self, student):
+        return self.students.filter_by(id=student.id).first() is not None
+
+    def assistant_join(self, assistant):
+        if not self.assistant_is_joined(assistant):
+            self.assistants.append(assistant)
+            db.session.commit()
+
+    def assistant_quit(self, assistant):
+        if self.assistant_is_joined(assistant):
+            self.assistants.remove(assistant)
+            db.session.commit()
+
+    def assistant_is_joined(self, assistant):
+        return self.assistants.filter_by(id=assistant.id).first() is not None
