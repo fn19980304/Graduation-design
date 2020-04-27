69db2005eb540e2ce368fc07bc7522e186952e8c
Jojof <496903741@qq.com>
2020-04-27 19:06:02 +0800

    修改讲师端页面为中文

--- devGrasys/forms/lecturer.py
+++ devGrasys/forms/lecturer.py
@@ -15,64 +15,64 @@ from devGrasys.models import Lecturer, Course
 
 
 class LoginFormLecturer(FlaskForm):
-    user_id = StringField('UserID', validators=[DataRequired(), Length(10, 10)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember me')
-    submit = SubmitField('Log in')
+    user_id = StringField('用户ID', validators=[DataRequired(), Length(10, 10)])
+    password = PasswordField('密码', validators=[DataRequired()])
+    remember_me = BooleanField('记住我')
+    submit = SubmitField('登录')
 
 
 class RegisterFormLecturer(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(1, 30)])
-    user_id = StringField('UserID', validators=[DataRequired(), Length(10, 10), Regexp('^[L][0-9]*$',
+    name = StringField('用户名称', validators=[DataRequired(), Length(1, 30)])
+    user_id = StringField('用户ID', validators=[DataRequired(), Length(10, 10), Regexp('^[L][0-9]*$',
                                                                                        message='Lecturer ID should '
                                                                                                'begin with L.')])
-    password = PasswordField('Password', validators=[DataRequired(), Length(8, 128), EqualTo('password2')])
-    password2 = PasswordField('Confirm password', validators=[DataRequired()])
-    submit = SubmitField()
+    password = PasswordField('密码', validators=[DataRequired(), Length(8, 128), EqualTo('password2')])
+    password2 = PasswordField('确认密码', validators=[DataRequired()])
+    submit = SubmitField('提交')
 
     def validate_user_id(self, field):
         if Lecturer.query.filter_by(user_id=field.data).first():
-            raise ValidationError('The ID is already in use.')
+            raise ValidationError('ID已经被其他用户注册！')
 
 
 class CreateCourseForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(1, 30)])
-    intro = TextAreaField('Introduction', validators=[DataRequired()])
-    submit = SubmitField('Create')
+    name = StringField('课程名称', validators=[DataRequired(), Length(1, 30)])
+    intro = TextAreaField('课程介绍', validators=[DataRequired()])
+    submit = SubmitField('创建')
 
     def validate_class_name(self, field):
         if Course.query.filter_by(name=field.data).first():
-            raise ValidationError('The class name is already in use.')
+            raise ValidationError('课程名已经被其他讲师使用！')
 
 
 class AdminCourseForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(1, 30)])
-    intro = TextAreaField('Introduction', validators=[DataRequired()])
-    submit = SubmitField()
+    name = StringField('课程名称', validators=[DataRequired(), Length(1, 30)])
+    intro = TextAreaField('课程介绍', validators=[DataRequired()])
+    submit = SubmitField('修改')
 
     def validate_class_name(self, field):
         if Course.query.filter_by(name=field.data).first():
-            raise ValidationError('The class name is already in use.')
+            raise ValidationError('课程名已经被其他讲师使用！')
 
 
 class AssignHomeworkForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(), Length(1, 30)])
-    description = CKEditorField('Description', validators=[DataRequired()])
-    deadline = DateField('Deadline', format='%Y-%m-%d')
-    submit = SubmitField('Assign')
+    title = StringField('作业标题', validators=[DataRequired(), Length(1, 30)])
+    description = CKEditorField('作业描述', validators=[DataRequired()])
+    deadline = DateField('截止日期', format='%Y-%m-%d')
+    submit = SubmitField('布置')
 
 
 class EditProfileFormLecturer(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(1, 30)])
-    submit = SubmitField()
+    name = StringField('用户名称', validators=[DataRequired(), Length(1, 30)])
+    submit = SubmitField('修改')
 
 
 class UploadAvatarFormLecturer(FlaskForm):
-    image = FileField('Upload', validators=[
+    image = FileField('上传', validators=[
         FileRequired(),
-        FileAllowed(['jpg', 'png'], 'The file format should be .jpg or .png.')
+        FileAllowed(['jpg', 'png'], '文件格式应为 .jpg 或 .png.')
     ])
-    submit = SubmitField()
+    submit = SubmitField('上传')
 
 
 class CropAvatarFormLecturer(FlaskForm):
@@ -80,4 +80,4 @@ class CropAvatarFormLecturer(FlaskForm):
     y = HiddenField()
     w = HiddenField()
     h = HiddenField()
-    submit = SubmitField('Crop and Update')
+    submit = SubmitField('裁剪并上传')
